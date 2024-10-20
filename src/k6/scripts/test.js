b55bbf517cc5db7a304ca6b4ed79f4b3fb441460
hugo_coisne <coisnehugo@gmail.com>
2024-10-20 16:56:26 -0400

    :sparkles: k6 functioning

--- src/k6/scripts/test.js
+++ src/k6/scripts/test.js
@@ -1,18 +1,13 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
-
-const BASE_URL = "http://localhost:8080";
+import { sleep } from "k6";
+const BASE_URL = "http://service:8080";
 
 export const options = {
-  vus: 10, // Number of virtual users
-  duration: '30s', // Duration of the test
+  vus: 1, // Number of virtual users
+  duration: '10s', // Duration of the test
 };
 
 export default function () {
   // Test the /rolldice endpoint
   const rollsResponse = http.get(`${BASE_URL}/rolldice?rolls=3`);
-  console.log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-  console.log(rollsResponse);
-
-  sleep(1); // Wait for 1 second between iterations
 }
